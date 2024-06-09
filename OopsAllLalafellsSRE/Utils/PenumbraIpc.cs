d4e0aaa1a5ae198d635e6a44a70977866dbfe90a
kelvin124124 <lindedi03@gmail.com>
2024-06-09 17:16:45 +0800

    Upgrade Penumbra ipc

---
+++ OopsAllLalafellsSRE/Utils/PenumbraIpc.cs
@@ -0,0 +1,40 @@
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Plugin;
+using Penumbra.Api.Enums;
+using Penumbra.Api.Helpers;
+using Penumbra.Api.IpcSubscribers;
+using System;
+
+namespace OopsAllLalafellsSRE.Utils
+{
+    internal class PenumbraIpc(DalamudPluginInterface pluginInterface) : IDisposable
+    {
+        private readonly RedrawAll redrawAll = new(pluginInterface);
+        private readonly RedrawObject redrawObject = new(pluginInterface);
+        private readonly EventSubscriber<nint, Guid, nint, nint, nint> creatingCharacterBaseEvent =
+            CreatingCharacterBase.Subscriber(pluginInterface, Drawer.OnCreatingCharacterBase);
+
+        public void Dispose()
+        {
+            creatingCharacterBaseEvent.Dispose();
+        }
+
+        internal void RedrawObject(GameObject gameObject, RedrawType setting)
+        {
+            redrawObject.Invoke(gameObject.ObjectIndex, setting);
+        }
+
+        internal void RedrawAll(RedrawType setting)
+        {
+            try
+            {
+                redrawAll.Invoke(setting);
+            }
+            catch (Exception ex)
+            {
+                Plugin.OutputChatLine($"Warning: Penumbra not found. Error: {ex.Message}\n" +
+                                      "Note: if you disable Penumbra before this plugin, lalafells will stay there until updated.");
+            }
+        }
+    }
+}
