5a0523965474fc375db92aaf90da3cda3e52f4b1
Senko <152493636+helpful-fox-senko-san@users.noreply.github.com>
2024-11-29 05:01:31 +0000

    OopsAllNaked

--- OopsAllLalafellsSRE/Configuration.cs
+++ OopsAllLalafellsSRE/Configuration.cs
@@ -9,10 +9,20 @@ namespace OopsAllLalafellsSRE
     internal class Configuration : IPluginConfiguration
     {
         public int Version { get; set; } = 0;
-        public Race SelectedRace { get; set; } = Race.LALAFELL;
+        public Race SelectedRace { get; set; } = Race.UNKNOWN;
         public bool enabled { get; set; } = false;
         public bool stayOn { get; set; } = false;
-        public bool nameHQ { get; set; } = true;
+        public bool nameHQ { get; set; } = false;
+
+        public bool dontStripSelf { get; set; } = false;
+        public bool dontLalaSelf { get; set; } = false;
+
+        public bool stripHats { get; set; } = true;
+        public bool stripBodies { get; set; } = true;
+        public bool stripLegs { get; set; } = true;
+        public bool stripGloves { get; set; } = true;
+        public bool stripBoots { get; set; } = true;
+        public bool stripAccessories { get; set; } = true;
 
         // the below exist just to make saving less cumbersome
         [NonSerialized]
