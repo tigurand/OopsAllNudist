945d6d7e967502ff46bbdcd97cf240d1c2b1a0dc
Lucille <t_grants@yahoo.com>
2025-03-29 03:24:35 +0800

    7.2 Update
    7.2 Update.

--- OopsAllNudist/Utils/Service.cs
+++ OopsAllNudist/Utils/Service.cs
@@ -20,6 +20,7 @@ internal class Service
     [PluginService] public static ICommandManager commandManager { get; private set; } = null!;
     [PluginService] public static IObjectTable objectTable { get; private set; } = null!;
     [PluginService] public static ITargetManager targetManager { get; private set; } = null!;
+    [PluginService] public static IFramework Framework { get; private set; } = null!;
 
     [PluginService] public static IPluginLog Log { get; private set; } = null!;
 }
